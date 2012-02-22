ebdd56cdbb4b4b6335a5d78e2af8759eaefd25c9
Miguel Vaz <Miguel.Vaz@d-fine.de>
2012-02-22 07:17:23 +0100

    added price calculation

--- finpypa/bonds.py
+++ finpypa/bonds.py
@@ -1,28 +1,45 @@
 #!/usr/bin/env python
 
+from scipy.optimize import fmin
+
 class Bond(object):
     def __init__(self, p=None, r=None, maturity=None):
         self.rate = r
         self.par = p
         self.maturity = maturity
-    
-    def value(self, cur_rate, t=0):
+
+    def get_rate(self, price, **kwargs):
+        def f_aux(rate):
+            return ( price - self.value(rate=rate) ) ** 2
+        return fmin( f_aux, self.rate, xtol=0.00000000001)[0]
+
+    def value(self, rate=None, t=0):
         pass
-        
+
 class ZeroCoupon(Bond):
     def __init__(self, p=1000, r=None, maturity=None, compound_fraction=1):
         super(ZeroCoupon, self).__init__(p=p, r=r, maturity=maturity)
         self.compound_fraction = compound_fraction
-    
+
     def value(self, rate=None, t=0):
         rate = rate or self.rate
         val = self.par * pow( 1 + rate/self.compound_fraction, - (self.maturity - t) * self.compound_fraction)
         return val
-    
+
     def profitLoss(self, rate=None, t=0):
-        rate = rate or self.rate
         return self.value(rate=rate, t=t) - self.value()
         # tmp = ZeroCoupon( self.par, rate, self.maturity - t, compound_fraction=self.compound_fraction)
         # return self.value() - tmp.value()
-        
-        
+
+
+class CouponBond(Bond):
+    def __init__(self, p=1000, compound=40, r=None, maturity=None, compound_fraction=1):
+        super(CouponBond, self).__init__(p=p, r=r, maturity=maturity)
+        self.compound_fraction = compound_fraction
+        self.number_fractions = maturity * compound_fraction
+        self.compound = compound
+
+    def value(self, rate=None, t=0):
+        rate = ( rate or self.rate ) / (1.0 * self.compound_fraction)
+        total_rates = self.compound_fraction * self.maturity
+        return self.compound / rate + (self.par - self.compound / rate) * pow(1 + rate, - total_rates)
--- finpypa/tests.py
+++ finpypa/tests.py
@@ -4,13 +4,19 @@ from nose.tools import assert_almost_equals
 def test_bond_value():
     b1 = bonds.ZeroCoupon(r=0.06, maturity=20)
     assert_almost_equals( b1.value(), 311.8, places=2)
-    
+
     b3 = bonds.ZeroCoupon(r=0.07, maturity=19.5, compound_fraction=2)
     assert_almost_equals( b3.value(), 261.41, places=2)
-    
+
 
 def test_profit_loss():
     b2 = bonds.ZeroCoupon(r=0.06, maturity=20, compound_fraction=2)
     assert_almost_equals( b2.value(), 306.56, places=2)
-    
-    assert_almost_equals( b2.profitLoss(rate=0.07, t=0.5), -45.14, places=2) 
+
+    assert_almost_equals( b2.profitLoss(rate=0.07, t=0.5), -45.14, places=2)
+
+def test_coupon_bond():
+    cb = bonds.CouponBond(r=0.04, maturity=30, compound=40, p=1000, compound_fraction=2)
+    assert_almost_equals( cb.value(rate=0.08), 1000.0)
+
+    assert_almost_equals( cb.get_rate(1200.0), 0.06, places=2)
